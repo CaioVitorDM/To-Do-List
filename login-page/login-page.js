0c387f33b635caa0a2f7d0fd7484cc35eddb6655
Caio Vitor <caiovitordm@protonmail.com>
2023-11-23 14:22:45 -0300

    feat: login-page e seus respectivos estilos

---
+++ login-page/login-page.js
@@ -0,0 +1,22 @@
+const container = document.getElementById('container');
+const registerBtn = document.getElementById('register');
+const loginBtn = document.getElementById('login');
+const loginBtn2 = document.getElementById('loginButton')
+
+registerBtn.addEventListener('click', ()=>{
+    container.classList.add("active");
+})
+
+loginBtn.addEventListener('click', ()=>{
+    container.classList.remove("active");
+})
+
+loginBtn2.addEventListener('click', ()=>{
+    loginMethod();
+})
+
+
+function loginMethod(){
+    console.log('Email: ' + document.getElementById('emailRegister').value);
+    console.log('Password: ' + document.getElementById('passwordRegister').value);
+}
